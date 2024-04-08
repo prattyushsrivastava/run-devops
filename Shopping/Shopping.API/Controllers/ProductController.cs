51f878992e23b487036d96d5f62d2c41357c1d26
prattyushsrivastava <prattyush.srivastava_cs19@gla.ac.in>
2024-04-08 16:06:05 +0530

    controller changes

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Models;
+using Shopping.Client.Data;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        public readonly ILogger<ProductController> _logger;
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            //var rn = new Random();
+            //return Enumerable.Range(1, 5).Select(Index => new Product
+            //{
+            //    Name = "asd"
+            //}).ToArray();
+            return ProductContext.Products;
+        }
+    }
+}
